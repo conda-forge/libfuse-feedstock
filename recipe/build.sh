e507ae604a676235bbcda21f246e5d4c2e86e528
Ben Mares <15216687+maresb@users.noreply.github.com>
2021-04-16 20:47:07 +0200

    sbin

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 
-export MOUNT_FUSE_PATH=${PREFIX}/bin
-export UDEV_RULES_PATH=${PREFIX}/etc/udev/rules.d
-export INIT_D_PATH=${PREFIX}/etc/init.d
+export MOUNT_FUSE_PATH="${PREFIX}/bin"
+export UDEV_RULES_PATH="${PREFIX}/etc/udev/rules.d"
+export INIT_D_PATH="${PREFIX}/etc/init.d"
 
 ./configure \
   --prefix="${PREFIX}" \
   --enable-static=no \
-  --sbindir=${PREFIX}/bin
+  --sbindir="${PREFIX}/bin" \
+;
 
 make
-
 make install
+
+mkdir -p "${PREFIX}/sbin"
+mv "${PREFIX}/bin/fusermount" "${PREFIX}/sbin/fusermount"
