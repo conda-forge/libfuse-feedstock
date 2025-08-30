2dfa9c1f41b361d812448c9432acf028a584b97e
Mark Harfouche <mark.harfouche@gmail.com>
2025-08-30 16:18:37 -0400

    Rename libfuse package to lifuse3 to allow co-installation with libfuse2

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,7 +5,8 @@ meson \
   --libdir=lib \
   --sbindir=bin \
   -Dexamples=false \
-  -Duseroot=false -Dudevrulesdir="${PREFIX}/lib/udev/rules.d" \
+  -Duseroot=false \
+  -Dudevrulesdir="${PREFIX}/lib/udev/rules.d" \
   build
 ninja -C build/
 ninja -C build/ install
